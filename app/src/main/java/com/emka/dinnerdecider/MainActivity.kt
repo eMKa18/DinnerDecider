a2c5b0a2a99ae76ac6b4cc47ee5e1ab03c31f910
Monika Koschel <mkoschel6@gmail.com>
2018-04-14 13:47:34 +0200

    Logic added

--- app/src/main/java/com/emka/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/emka/dinnerdecider/MainActivity.kt
@@ -3,14 +3,26 @@ package com.emka.dinnerdecider
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "McDonalds", "Barros Pizza")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodText.text = "Burger King"
+        decideButton.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFood]
+        }
 
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+        }
     }
 }
